c144d2283644cb7ad54b10d656efd9a05bd61e2a
Noah Lee <thenoah54@gmail.com>
2023-12-10 22:01:59 -0500

    Updated,make grading easier (may have missed some)

--- classification.py
+++ classification.py
@@ -3,7 +3,8 @@ import sys
 creates the date to be found as well as sets up sys.argv
 """
 def set_date():
-    try: 
+    try:
+        # 1.6 
         set_date = sys.argv[1]
     except:
         set_date = input("Enter a date (yyyy-mm-dd): ")
@@ -20,4 +21,5 @@ def artist_song_tuple(list1, list2):
         item1.append(list1[i])
         item2.append(list2[i])
     # print(item1, item2)
+    # 5.14
     return (item1, item2)
--- final.py
+++ final.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from pprint import pprint
 import os
+# 3.17/3.18
 import classification
 
 # custome error message 
@@ -16,10 +17,13 @@ if len(set_date) != 10:
 
 # test
 # response = requests.get("https://www.billboard.com/charts/hot-100/2023-11-25/")
+# 10.1
 response = requests.get(f"https://www.billboard.com/charts/hot-100/{set_date}/")
 webpage = response.text
+# 10.2
 soup = BeautifulSoup(webpage, "html.parser")
 
+# 10.3
 # finds the title using CSS selectors. h3 in a li in a ul in a li tag
 title_tags = soup.select(selector="li ul li h3")
 # because the number one song's class is different for some reason
@@ -28,10 +32,13 @@ artist_tags = soup.find_all(name="span", class_="c-label a-no-trucate a-font-pri
 
 # print(title_tags)
 # print(len(title_tags))
+# 5.7
 songs = []
 artists = []
 for title in title_tags:
+    # 5.9
     songs.append(title.getText().strip())
+# 5.8
 artists.append(number_one_artist.getText().strip())
 for artist in artist_tags:
     artists.append(artist.getText().strip())
@@ -41,6 +48,7 @@ try:
         None
 except FileNotFoundError:
     print("Creating new file...")
+    # 3.20
     with open("songs.txt", 'w') as file:
         None
 
@@ -48,12 +56,14 @@ except FileNotFoundError:
 # for i in range(len(songs)):
 #     song_artist.append(classification.artist_song_tuple(songs, artists))
 
+# 5.15
 songs_100 = {}
 counter = 1
 date = True
 if os.path.exists("songs.txt") == True:
     try:
         for i in range(0, 100):
+            # 5.17/5.18/5.19/5.13
             songs_100[counter] = classification.artist_song_tuple(songs, artists)[0][i]
             # writes the date once
             while date:
@@ -61,9 +71,11 @@ if os.path.exists("songs.txt") == True:
                     file.write(f"###### {set_date} ######\n")
                     date = False
             with open("songs.txt", 'a') as file:
+                # 3.21
                 file.write(f"{counter}: {classification.artist_song_tuple(songs, artists)[0][i]} by, {classification.artist_song_tuple(songs, artists)[1][i]}\n")
             counter += 1
         # print(set_date)
         # pprint(songs_100) 
     except IndexError:
+        # 4.9
         CustomError("Invalid Date")
